d3cc76974a836b5779baf290a245e3f3f4e69ca5
darazsimate <darazsi.mate@gmail.com>
2022-09-19 20:09:13 +0200

    kód javítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // " " ;
-    for (int i = 0;) // ciklus feltétel hiányos
+    int *b = new int[N_ELEMENTS]; // N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // " " ;
+    for ( int i = 1; i <= N_ELEMENTS; i++) // ciklus feltétel hiányos
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // ciklus feltétel hibás
+    for ( int i = 1; i <= N_ELEMENTS; i++) // ciklus feltétel hibás
     {
-        std::cout << "Ertek:" // pontos vessző
+        std::cout << "Ertek:" << std::endl; // pontos vessző, endl
+        std::cout << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag; 
-    for (int i = 0; i < N_ELEMENTS, i++)  // ; helyett , 
+    for ( int i = 1; i <= N_ELEMENTS; i++)  // ; helyett , 
     {
-        atlag += b[i] // pontos vessző hiányzik
+        atlag += b[i]; // pontos vessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
